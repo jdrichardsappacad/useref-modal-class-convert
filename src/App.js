9540b5003a22a0443f544e49244f5f4656d61e0f
James Richards <jd-app@AppAcademyAir.home>
2021-01-09 07:30:42 -0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,46 @@
-function App() {
+import React, { useState } from 'react';
+import Modal from './modal2.js';
+
+export default function App() {
+  const [isModalOpen, setModalOpen] = useState(false);
+  const [quantity, setQuantity] = useState(6);
+
+  const price = 20;
+  const onClose = () => setModalOpen(false);
+  const onSubmit = value => setQuantity(value);
   return (
-    <h1>Hello from App</h1>
+    <div className='app'>
+      <h1>Checkout</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>Quantity</th>
+            <th>Unit price</th>
+            <th>Total</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>{quantity}</td>
+            <td>${price}</td>
+            <td>${price * quantity}</td>
+            <td>
+              <button
+                onClick={e => {
+                  e.preventDefault();
+                  e.stopPropagation();
+                  setModalOpen(!isModalOpen);
+                }}
+              >
+                Edit quantity
+              </button>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+      {isModalOpen && (
+        <Modal onSubmit={onSubmit} initialValue={quantity} onClose={onClose} />
+      )}
+    </div>
   );
 }
-
-export default App;
